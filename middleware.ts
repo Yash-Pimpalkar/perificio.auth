6b334ca22ccd6f91642952580a15b6f3b3f42a8d
YashPimpalkar <yashpimpalkar214@gmail.com>
2025-06-22 18:20:42 +0530

    route protected

--- middleware.ts
+++ middleware.ts
@@ -8,26 +8,27 @@ const protectedRoutes = ["/dashboard", "/settings", "/profile"];
 
 
 export default async function middleware(request: NextRequest) {
-   const session = await auth();
-   const adminRoutes = ["/dashboard", "/admin", "/upload-pdf"];
-  const url = request.nextUrl;
+    const session = await auth();
+    const adminRoutes = ["/dashboard", "/admin", "/upload-pdf"];
+    const url = request.nextUrl;
+    const isAdmin = session?.user?.role === "ADMIN" || session?.user?.role === "SUPERADMIN";
 
-   
-    const isProtectedRoute = protectedRoutes.some((route) =>
+
+    const isAdminProtected = adminRoutes.some((route) =>
         request.nextUrl.pathname.startsWith(route)
     );
-     const isAdminProtected = adminRoutes.some((route) =>
-        request.nextUrl.pathname.startsWith(route)
-    );
-    
-    
-    if(!session && isAdminProtected) {
-        const absoluteUrl =  new URL("/sign-in",request.nextUrl.origin);
-        return NextResponse.redirect(absoluteUrl.toString());
+
+
+    if (isAdminProtected) {
+        if (!session || !isAdmin) {
+            const absoluteUrl = new URL("/sign-in", request.nextUrl.origin);
+            return NextResponse.redirect(absoluteUrl.toString());
+        }
     }
+
     return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 }
