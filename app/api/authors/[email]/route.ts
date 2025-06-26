65f8ec8e881e60e23b5d8ac5a7e6be63db721232
YashPimpalkar <yashpimpalkar214@gmail.com>
2025-06-27 00:01:20 +0530

    asda

--- app/api/authors/[email]/route.ts
+++ app/api/authors/[email]/route.ts
@@ -1,23 +1,23 @@
-import { prisma } from "@/db";
-import { NextResponse } from "next/server";
+// import { prisma } from "@/db";
+// import { NextResponse } from "next/server";
 
 
-export async function GET(
-  req: Request,
-  { params }: { params: { email: string } }
-) {
-  try {
-    const email = params.email;
-    const posts = await prisma.user.findUnique({
-      where: { email },
-      include: {
-        posts: { orderBy: { createdAt: "desc" } },
-      },
-    });
+// export async function GET(
+//   req: Request,
+//   { params }: { params: { email: string } }
+// ) {
+//   try {
+//     const email = params.email;
+//     const posts = await prisma.user.findUnique({
+//       where: { email },
+//       include: {
+//         posts: { orderBy: { createdAt: "desc" } },
+//       },
+//     });
 
-    return NextResponse.json(posts);
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json({ message: "Could not fetch post" });
-  }
-}
+//     return NextResponse.json(posts);
+//   } catch (error) {
+//     console.log(error);
+//     return NextResponse.json({ message: "Could not fetch post" });
+//   }
+// }
